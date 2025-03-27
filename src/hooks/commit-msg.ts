c5914a86efad0a79e7010d679989519a5c8db644
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2025-03-27 03:04:56 +0000

    type chore chore: Test commit7.

---
+++ src/hooks/commit-msg.ts
@@ -0,0 +1,37 @@
+const commit_types = [
+	'build', 
+	'chore', 
+	'ci', 
+	'docs', 
+	'style', 
+	'refactor', 
+	'perf', 
+	'test', 
+	'feat', 
+	'fix', 
+	'revert'
+]
+const commit_message = process.argv[2] + ''
+
+const formatMessage = (commit_message: string) => {
+	const message = commit_message.trim()
+	const splitted = message.split(':')
+	const type = (splitted[0] || 'chore').trim()
+	console.log('type', type)
+	if (!commit_types.includes(type)) {
+		throw new Error(`Invalid commit type: ${type}`)
+	}
+	let subject = splitted.slice(1).join(':').trim()
+	subject = subject.replace(/\s+/g, ' ') // Ensure no more than one space
+	subject = subject.charAt(0).toUpperCase() + subject.slice(1) // Capitalize first letter
+	if (!subject.endsWith('.')) {
+		subject += '.' // Add period if missing
+	}
+    
+	const formatted_message = `${type}: ${subject}`
+    
+	return formatted_message
+}
+const formatted_message = formatMessage(commit_message)
+
+process.stdout.write(formatted_message)
